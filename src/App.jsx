2387a1a46d4f523d515bc822d317ca871d75e39c
Rajesh Kumar Pasi <imvip2002@gmail.com>
2026-08-26 13:28:04 +0530

    search Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import { fetchUnsplashImages,fetchPexelsImages } from "./api/mediaApi";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
   return (
-    <div className="h-screen text-white w-full bg-gray-950 flex flex-row items-center gap-2 justify-center">
-      <button
-      className="bg-blue-500 px-4 py-2 rounded-md"
-        onClick={async () => {
-          const response = await fetchUnsplashImages("nature");
-          console.log(response.data.results);
-        }}
-      >
-        Get Images
-      </button>
-      <button
-        className="bg-green-500 px-4 py-2 rounded-md"
-        onClick={async () => {
-          const response = await fetchPexelsImages("health");
-          console.log(response.data.videos);
-        }}
-      >
-        Get Videos
-      </button>
+    <div className="h-screen text-white w-full bg-gray-950">
+      <SearchBar />
     </div>
   );
-};
+}
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setQuery } from "../utilities/features/searchSlice";
 
 const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
-}
+  const [searchTerm, setSearchTerm] = useState("");
 
-export default SearchBar
+  const Dispatch = useDispatch();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    Dispatch(setQuery(searchTerm));
+  };
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          handleSearch(e);
+        }}
+        className="flex  bg-(--c1) gap-5 py-10 px-10"
+      >
+        <input
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+          required
+          className="w-full border-2 px-6 py-3 text-xl rounded outline-none"
+          type="text"
+          placeholder="Search anything..."
+        />
+        <button className="active:scale-95 cursor-pointer border-2 px-6 py-3 text-xl rounded outline-none">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
